94e113c82619af66c3ff89cf658da7137f3106f9
Bartholkayeye <barthokdiaz.nkulukayeye@gmail.com>
2022-06-07 03:53:31 -0700

    Tasks 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,17 +2,4 @@
 #définir MAIN_H_INCLUDED
 
 int _putchar(char c);
-print_alphabet nul(void);
-print_alphabet_x10 nul(nul));
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int n);
-jack_bauer nul(nul));
-times_table nul(void);
-int add(int a, int b);
-null print_to_98(int n);
-print_times_table nul(int n);
-
 #endif 
