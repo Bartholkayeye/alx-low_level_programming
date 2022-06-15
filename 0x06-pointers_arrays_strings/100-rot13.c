e5cdc08164ec91f103110a9568b47e2d1ebd7cd0
Bartholkayeye <104756667+Bartholkayeye@users.noreply.github.com>
2022-06-15 23:53:49 +0200

    Tasks 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,15 +10,15 @@ char *rot13(char *s)
 int i;
 char *p = s;
 
-char *alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char *original = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char *code = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 while (*s != '\0')
 {
 	i = 0;
 	while (i < 53)
 	{
-		if (*s == alphabet[i])
+		if (*s == original[i])
 	{
 			*s = code[i];
 				break;
