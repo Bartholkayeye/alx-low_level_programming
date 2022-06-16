7c93ddd956685fffaa479f8dec86733dcdc416d3
Bartholkayeye <104756667+Bartholkayeye@users.noreply.github.com>
2022-06-17 00:01:04 +0200

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,28 +7,37 @@
  */
 int main(void)
 {
-	int digit1, digit2, digit3;
+int c;
+int d;
+int e = 0;
 
-	for (digit1 = 0; digit1 < 8; digit1++)
-	{
-		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
-		{
-			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
-			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
+while (e < 10)
+{
+d = 0;
+while (d < 10)
+{
+c = 0;
+while (c < 10)
+{
+if (c != d && d != e && e < d && d < c)
+{
+putchar('0' + e);
+putchar('0' + d);
+putchar('0' + c);
 
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;
-				
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
+if (c + d + e != 9 + 8 + 7)
+{
+putchar(',');
+putchar(' ');
+}
+}
 
-	putchar('\n');
-
-	return (0);
+c++;
+}
+d++;
+}
+e++;
+}
+putchar('\n');
+return (0);
 }
