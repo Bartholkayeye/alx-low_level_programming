186f1616cf2d88585e3747e8d38918d3d8b41623
Bartholkayeye <104756667+Bartholkayeye@users.noreply.github.com>
2022-06-20 13:07:57 +0200

    Tasks 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,16 +3,21 @@
 /**
  * print_chessboard - prints chessboard
  * @a: double pointer
- * Return: 0
+ * Return: char
  */
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i, j;
-
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-			_putchar('\n');
-	}
+int b, c;
+b = 0;
+while (b < 8)
+{
+c = 0;
+while (c < 8)
+{
+_putchar (a[b][c]);
+c++;
+}
+_putchar ('\n');
+b++;
+}
 }
