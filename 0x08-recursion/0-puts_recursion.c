8a195168d50eb7fd15bb0c8c2017124229f12032
Bartholkayeye <104756667+Bartholkayeye@users.noreply.github.com>
2022-06-21 19:44:44 +0200

    Tasks 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,13 +4,15 @@
 * @s: string
 * Return: Always (0)
 */
+
 void _puts_recursion(char *s)
 {
   if (*s == '\0')
-    _putchar('\n');
-  else
-  {
-    _putchar(*s);
-    _puts_recursion(++s);
-  }
+    {
+    _putchar ('\n');
+    return;
+    }
+  _putchar (*s);
+  s++;
+  _puts_recursion(s);
 }
