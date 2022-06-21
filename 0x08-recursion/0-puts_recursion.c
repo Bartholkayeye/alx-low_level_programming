15407cb839fc16a6af18b4e82ddd760821e8371b
Bartholkayeye <104756667+Bartholkayeye@users.noreply.github.com>
2022-06-21 19:38:08 +0200

    Tasks 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _puts_recursion - prints a string folowed by a new line
- * @s: string
- * Return: Always (0)
- */
+* _puts_recursion - prints a string folowed by a new line
+* @s: string
+* Return: Always (0)
+*/
 void _puts_recursion(char *s)
 {
   if (*s == '\0')
