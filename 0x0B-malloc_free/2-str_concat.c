91bb7eadaa9564390a41d2f466810956abedfe93
Bartholkayeye <104756667+Bartholkayeye@users.noreply.github.com>
2022-06-26 14:08:49 +0200

    Tasks 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 /**
- *_strlen - count arrray
- *@s: array of elements
- *Return: i
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to newly allocated space
  */
 char *str_concat(char *s1, char *s2)
 {
