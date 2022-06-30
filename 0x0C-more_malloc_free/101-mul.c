05cd31710ad0602a12b7f735074c43cc8d2ebf03
Bartholkayeye <104756667+Bartholkayeye@users.noreply.github.com>
2022-06-30 16:32:06 +0200

    Tasks 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,91 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * _puts - prints a string, followed by a new line,
- * @str: pointer to the string to print
- * Return: void
-*/
-
-
-void _puts(char *str)
-{
-int i = 0;
-while (str[i])
-{
-	_putchar(str[i]);
-	i++;
-}
-
-}
 
 /**
- * _atoi - convert a string to an integer.
- * @s: char type string
- * Return: integer converted
+ * _realloc - allocate memory and set all values to 0
+ * @ptr: pointer to the memory previously allocated (malloc(old_size))
+ * @old_size: size previously allocated
+ * @new_size: new size to reallocate
+ * Return: pointer to reallocated memory
  */
 
-int _atoi(const char *s)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	int sign = 1;
-	unsigned long int resp = 0, firstNum, i;
+	void *p;
+	unsigned int i;
 
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	if (new_size == 0 && ptr != NULL) /* free memory if reallocate 0 */
 	{
-		if (s[firstNum] == '-')
-		{
-			sign *= -1;
-		}
+		free(ptr);
+		return (NULL);
 	}
 
-	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+	if (new_size == old_size) /* return ptr if reallocating same old size */
+		return (ptr);
+
+	if (ptr == NULL) /* malloc new size if ptr is originally null */
 	{
-		resp *= 10;
-		resp += (s[i] - 48);
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		else
+			return (p);
 	}
 
-	return (sign * resp);
-}
+	p = malloc(new_size); /* malloc and check error */
+	if (p == NULL)
+		return (NULL);
 
-/**
- * print_int - prints an integer.
- * @n: int
- * Return: 0
- */
+	/* fill up values up till minimum of old or new size */
+	for (i = 0; i < old_size && i < new_size; i++)
+		*((char *)p + i) = *((char *)ptr + i);
+	free(ptr); /* free old ptr */
 
-void print_int(unsigned long int n)
-{
-
-unsigned  long int divisor = 1, i, resp;
-
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
-;
-
-for (; divisor >= 1; n %= divisor, divisor /= 10)
-{
-	resp = n / divisor;
-	_putchar('0' + resp);
-}
-
-}
-
-/**
- * main - print the result of the multiplication, followed by a new line
- * @argc: int
- * @argv: list
- * Return: 0
- */
-
-int main(int argc, char const *argv[])
-{
-(void)argc;
-
-if (argc != 3)
-{
-	_puts("Error ");
-	exit(98);
-}
-print_int(_atoi(argv[1]) * _atoi(argv[2]));
-_putchar('\n');
-
-return (0);
+	return (p);
 }
